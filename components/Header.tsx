422cbb7d96d3356d7f38e0e133e076cfe2b60db3
ipeleng-dev <ipelengsilas@yahoo.com>
2022-01-25 13:58:19 +0200

    header update

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,12 @@ function Header() {
   return (
     <header>
         {/*Left*/}
-        <div>
-            <Image src='https://links.papareact.com/qd3' layout='fill' />
+        <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+            <Image 
+            src='https://links.papareact.com/qd3' 
+            layout='fill' 
+            objectFit='contain'
+            objectPosition='left'/>
         </div>
 
         {/*Middle*/}
