fb6ebfe0cd8c75e8f19973405cb6e82733e2f1b9
ipeleng-dev <ipelengsilas@yahoo.com>
2022-01-25 14:16:00 +0200

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 function Header() {
   return (
-    <header className='sticky top-0 z-50 grid-col-3 bg-white shadow-md py-5 px-5'>
+    <header className='sticky top-0 z-50 grid-col-3 bg-white shadow-md p-5 md:px-10'>
         {/*Left*/}
         <div className='relative flex items-center h-10 cursor-pointer my-auto'>
             <Image 
@@ -15,7 +15,9 @@ function Header() {
         </div>
 
         {/*Middle*/}
-        <div></div>
+        <div>
+            <input type="text"/>
+        </div>
 
         {/*Right*/}
         <div></div>
