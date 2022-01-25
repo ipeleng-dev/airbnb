ae3869ca75164588c576d59e73a8646a617861ba
ipeleng-dev <ipelengsilas@yahoo.com>
2022-01-25 22:39:48 +0200

    finish build

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Banner from '../components/Banner'
 import Smallcard from '../components/Smallcard'
 import MediumCard from '../components/MediumCard'
 import LargeCard from '../components/LargeCard'
-
+import Footer from '../components/Footer'
 
 
 export default function Home({exploreData, cardsData}) {
@@ -57,6 +57,8 @@ export default function Home({exploreData, cardsData}) {
         buttonText='Get Inspired'
         />
       </main>
+
+      <Footer />
     </div>
   )
 }
