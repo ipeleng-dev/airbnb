748dc9370b3b5b6380847205dea721c3acfeb125
ipeleng-dev <ipelengsilas@yahoo.com>
2022-01-25 14:53:11 +0200

    searchbar update

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ function Header() {
 
         {/*Middle*/}
         <div className='flex items-center rounded-full py-2 md:border-2 md:shadow-sm'>
-            <input className='flex-grow pl-5 bg-transparent outline-none' type="text" placeholder='Start your search'/>
+            <input className='flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400' type="text" placeholder='Start your search'/>
             <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
         </div>
 
