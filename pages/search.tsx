3e536d4a8a1cd15c830fb3ebcdc99dc27fa3b322
ipeleng-dev <ipelengsilas@yahoo.com>
2022-01-26 15:53:29 +0200

    site changes

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,15 +1,28 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import {format} from 'date-fns'
 
 function Search() {
+
+    const router = useRouter();
+
+    //Es6 distructuring
+    const {location, startDate, endDate, noOfGuest} = router.query;
+
+    const formatedStartDate = format(new Date(startDate),"dd MMMM yy");
+    const formatedEndDate = format(new Date(endDate),"dd MMMM yy");
+
+    const range =`${formatedStartDate} - ${formatedEndDate}`;
+
   return (
     <div className="h-screen">
       <Header/>
       <main className='flex'>
           <section className='flex-grow pt-14 px-6'>
-              <p className='text-xs'>300+ Stays for 5 number of guests</p>
-              <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in Mars</h1>
+              <p className='text-xs'>300+ Stays - {range} -{noOfGuest} guests</p>
+              <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in {location}</h1>
 
               <div className='hidden lg:inline-flex whitespace-nowrap space-x-3 text-gray-800'>
                   <p className='button'>Cancelation Flexibility</p>
