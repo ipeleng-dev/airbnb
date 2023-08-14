3334a6880d5598c57f2fb1e0e12f085ee2714682
ipeleng-dev <ipelengsilas@yahoo.com>
2023-08-14 11:36:37 +0200

    project upload

---
+++ components/Banner.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+function Banner(){
+    return(
+        <div>
+            <Image src='https://links.papareact.com/0fm' alt='banner image'/>
+        </div>
+    )
+}
+export default Banner
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import Head from 'next/head';
 import Header from '../../components/Header'
+import Banner from "../../components/Banner";
 export default function Home() {
   return (
     <div className="">
         <Head>
-            <title>AIRBNB</title>
+            <title>Airbnb</title>
             <link rel="icon" href="https://seeklogo.com/images/A/airbnb-logo-1D03C48906-seeklogo.com.png"/>
         </Head>
     <Header/>
-    {/* Banner */}
+    <Banner/>
     </div>
   )
 }
