76e7b997976d9d1e0de575c090fedff44a378b7d
ipeleng-dev <ipelengsilas@yahoo.com>
2023-08-14 17:30:41 +0200

    project upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Header from '../../components/Header'
 import Banner from "../../components/Banner";
 import SmallCard from "../../components/SmallCard";
 import MediumCard from "../../components/MediumCard";
+import LargeCard from "../../components/LargeCard";
 
 export const getStaticProps = async ()=>{
 
@@ -55,6 +56,13 @@ export default function Home({exploreData, cardsData}) {
                 </div>
 
             </section>
+
+            <LargeCard
+                img='https://links.papareact.com/4cj'
+                title='The Greatest Outdoors'
+                description='Wishlists curated by Airbnb'
+                buttonText='Get Inspired'
+            />
         </main>
     </div>
   );
