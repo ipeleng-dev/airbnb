93c61045bfd167f604344b1b2a064202cffcb8b5
ipeleng-dev <ipelengsilas@yahoo.com>
2023-08-14 11:58:30 +0200

    project upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,21 @@ export default function Home() {
             <title>Airbnb</title>
             <link rel="icon" href="https://seeklogo.com/images/A/airbnb-logo-1D03C48906-seeklogo.com.png"/>
         </Head>
-    <Header/>
-    <Banner/>
+        <Header/>
+        <Banner/>
+
+        <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+            <section>
+                <h2 className='text-4xl font-semibold pb-5 pt-6'>Explore Nearby</h2>
+
+            {/*  pull data from server  */}
+
+            </section>
+        </main>
     </div>
   )
+}
+
+export async function getStaticProps(){
+
 }
