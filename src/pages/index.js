7ef64b3c35c688bfffc4b16ed4475f15fafc499f
ipeleng-dev <ipelengsilas@yahoo.com>
2023-08-14 13:46:23 +0200

    project upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,19 @@
+import  { NextPage } from 'next'
 import Head from 'next/head';
 import Header from '../../components/Header'
 import Banner from "../../components/Banner";
-export default function Home() {
+
+export const getStaticProps = async ()=>{
+
+    const res = await fetch('https://api.npoint.io/bb43f52dcf58170e29df');
+    const data = await res.json();
+
+    return{
+        props: {ninjas: data}
+    }
+}
+
+export default function Home({ninjas}) {
   return (
     <div className="">
         <Head>
@@ -16,13 +28,18 @@ export default function Home() {
                 <h2 className='text-4xl font-semibold pb-5 pt-6'>Explore Nearby</h2>
 
             {/*  pull data from server  */}
-
+                {ninjas.map(item =>( <h1>{item.location}</h1>))}
             </section>
         </main>
     </div>
-  )
+  );
 }
 
-export async function getStaticProps(){
-
-}
+// export async function getStaticProps(){
+//     const exploreData = await fetch("https://links.papareact.com/pyp").then((res) => res.json());
+//     return{
+//         props:{
+//             exploreData,
+//         },
+//     };
+// }
