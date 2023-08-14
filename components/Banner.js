2d0cb6e71ad83b9cedee4538d52a95db8371814b
ipeleng-dev <ipelengsilas@yahoo.com>
2023-08-14 11:37:17 +0200

    project upload

--- components/Banner.js
+++ components/Banner.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 function Banner(){
     return(
         <div>
-            <Image src='https://links.papareact.com/0fm' alt='banner image'/>
+            <Image src='https://links.papareact.com/0fm' alt='banner image' layout='fill'/>
         </div>
     )
 }
